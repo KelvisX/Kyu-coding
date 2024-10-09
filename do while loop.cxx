f8c07f5abd46074d095b62781a99657a51f2390c
KelvisX <kellywekesa3@gmail.com>
2024-10-09 07:43:02 +0300

    Add files via upload

---
+++ do while loop.cxx
@@ -0,0 +1,14 @@
+//do while loop
+#include <stdio.h>
+
+int main()
+{
+    int i=0, sum=0;
+    do{
+    	printf("%d \n",i);
+    	i++;
+    	sum =i;
+    }while(i<=50);
+}
+   	
+
---
+++ for loop output numbers 1-30.cxx
@@ -0,0 +1,12 @@
+//for loop - output numbers 1-30
+#include<stdio.h>
+
+int main() {
+    int i, sum=0;
+    for(i=15;i>0;i--) {
+        printf("%d \n", i*iy);
+        sum = sum + i;
+    }
+    printf("The sum is %d ", sum);
+      return 0;
+}
